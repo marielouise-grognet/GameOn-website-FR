075c7e1197451a8c89c2c2f37f48c6f68cf84d60
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-03-04 22:00:54 +0100

    ajout fonction fermeture de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+btnClose.addEventListener("click", closeModal);
+
+function closeModal () {
+  modalbg.style.display = "none";
+}
+
+
 
 
